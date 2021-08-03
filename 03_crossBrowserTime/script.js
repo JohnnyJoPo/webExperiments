8237796ce46a80112c7262c4dc23d02a7059cfa3
JohnnyJoPo <79624375+JohnnyJoPo@users.noreply.github.com>
2021-08-03 11:54:45 -0400

    Updated 03_crossBrowserTime

--- 03_crossBrowserTime/script.js
+++ 03_crossBrowserTime/script.js
@@ -15,22 +15,33 @@ function setTimes(){
     let sysData = document.getElementsByClassName("sysData");
     let selData = document.getElementsByClassName("selData");
     let inDate = document.getElementById("selectDate");
-    console.log(typeof(inDate.value));
 
     let today = new Date();
-    let selected = new Date(inDate.value);
+    let selectedTemp = new Date(inDate.value);
+    let offset = selectedTemp.getTimezoneOffset();
+    let selected = new Date(Date.parse(selectedTemp) + (offset * 60000));
 
+    let newSysDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    let sysOffset = today.getTimezoneOffset();
     sysData[0].innerText = today;
     sysData[1].innerText = today.getDate();
-    sysData[2].innerText = today.getMonth();
+    sysData[2].innerText = today.getMonth() + 1;
     sysData[3].innerText = today.getFullYear();
-    sysData[4].innerText = today.getTimezoneOffset();
+    sysData[4].innerText = sysOffset;
+    sysData[5].innerText = newSysDate;
+    sysData[6].innerText = `${Date.parse(newSysDate) + (sysOffset * 60000)}`;
+    sysData[7].innerText = new Date(Date.parse(newSysDate) + (sysOffset * 60000));
     
+    let newSelDate = `${selected.getFullYear()}-${selected.getMonth() + 1}-${selected.getDate()}`;
+    let selOffset = selected.getTimezoneOffset();
     selData[0].innerText = selected;
     selData[1].innerText = selected.getDate();
-    selData[2].innerText = selected.getMonth();
+    selData[2].innerText = selected.getMonth() + 1;
     selData[3].innerText = selected.getFullYear();
-    selData[4].innerText = selected.getTimezoneOffset();
+    selData[4].innerText = selOffset;
+    selData[5].innerText = newSelDate;
+    selData[6].innerText = `${Date.parse(newSelDate) + (selOffset * 60000)}`;
+    selData[7].innerText = new Date(Date.parse(newSelDate) + (selOffset * 60000));
 }
 
 if (window.addEventListener){
